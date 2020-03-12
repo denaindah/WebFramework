6e86fae4b79d83a799bfd975ef31601d2af362e8
denaindah <denaindahamalia@gmail.com>
2020-03-12 12:53:41 +0700

    pertemuan5

---
+++ pertemuan-5/JawabanTugas.md
@@ -0,0 +1,5 @@
+# Jawaban Pertanyaan Pertemuan 5
+
+## Praktikum 1
+a. 1. dena 
+b. 
---
+++ pertemuan-5/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import App from './App';
+import * as serviceWorker from './serviceWorker';
+// import HelloComponent from './component/HelloComponent';
+// import Statefullcomponent from './container/StateFullComponent';
+
+import BlogPost from './container/BlogPost/BlogPost';
+
+//function HelloWorld(){
+//    return <p> ini adalah function component</p>
+//}
+
+
+//const HelloWorld = () =>{
+//    return <p> ini adalah arrow function</p>
+//}
+
+//class Statefullcomponent extends React.Component
+//{
+//    render (){
+//        return <p> ini adalah statefull component</p>
+//    }
+//}
+
+ReactDOM.render(<BlogPost />, document.getElementById('content'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
